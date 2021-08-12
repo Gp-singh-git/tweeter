f337dc8b2abea1b829451071b163c9da225498c8
gurpreet singh <gurpreet212004@gmail.com>
2021-08-12 09:57:32 +0000

    icon updated

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,53 +3,7 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-// const tweetData = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png",
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1620757319000
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd"
-//     },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1628360519000
-//   },
-//   {
-//     "user": {
-//       "name": "Josephk",
-//       "avatars": "https://i.imgur.com/73hZDYK.png",
-//       "handle": "@Joseph123"
-//     },
-//     "content": {
-//       "text": "If friendship is your weakest point, you are the strongest person in the world"
-//     },
-//     "created_at": 1133979719000
-//   },
-//   {
-//     "user": {
-//       "name": "Monica",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@MiscMonica"
-//     },
-//     "content": {
-//       "text": "If you really want something, the whole universe starts working to achieve it for you."
-//     },
-//     "created_at": 1602094919000
-//   }
-// ];
-
+// 
 
 const createTweetElement = function(data) {
   // console.log("i am in",  data, data["user"], data.user);
@@ -104,23 +58,22 @@ $(`#form-tweet`).submit(function (event) {
   event.preventDefault();
   const check = $("#tweet-text").val();
   if(check.length>140) {
+    $(`#error-msg`).text("Please dont Exceed the 140 character limit");
+    $(`#error-msg`).append(`<i class="fas fa-exclamation-circle"></i> `);
     $(`#error-msg`).slideDown("slow");
   } else if (check.length === 0){
     $(`#error-msg`).text("Please enter some data to post tweet");
+    $(`#error-msg`).append(`<i class="fas fa-exclamation-circle"></i> `);
     $(`#error-msg`).slideDown("slow");
 
   } else {
     $(`#error-msg`).hide();
 
     let newTweetData = $("#tweet-text").serialize();
-    // console.log(newTweetData);
-
-    // newTweetData = `<p>${escape(newTweetData)}</p>`;
-    // const newTweetData = `<p>${escape($("#tweet-text"))}.serialize()</p>`;
     console.log(newTweetData);
     const url = "/tweets";
     $.ajax({ url: url, method:'POST', data: newTweetData})
-      .then(function (data_new) { 
+      .then(function () { 
           console.log("successfully posted: ");
           $.ajax({url:url, method:'GET'})
             .then(function(data_new) {
@@ -137,7 +90,6 @@ const loadTweets = function() {
   const url = "/tweets";
   $.ajax({url:url, method:'GET'})
   .then(function(newData) {
-    // console.log("New data", newData);
     rendertweets(newData);
   });
 }
